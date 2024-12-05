e0c1b9882dec24ab9f6a34161d245ec4fde6a45b
Ngetich-86 <gideonkngetich86@gmail.com>
2024-12-05 08:46:15 +0300

    worked on drizzle ORM

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,6 +1,23 @@
+import {Hono} from 'hono';
+import "dotenv/config";
+import {serve} from "@hono/node-server";
+import {cors} from "hono/cors";
 
-async function nameSalute(name:string) {
-    console.log(`hello my name is ${name}`)
-    } 
- 
-    nameSalute("ngetich");
+
+//routes
+
+const app = new Hono();
+
+app.use('*',cors());
+
+app.get('/',async(c)=>{
+    return c.json({ message: '🌟 Welcome to my API! 🚀' });
+
+});
+
+serve({
+    fetch: app.fetch,
+    port: Number(process.env.PORT)
+})
+
+console.log(`Server is running🚀 on http://localhost:${process.env.PORT} 🌍🎉`)
