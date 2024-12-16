8c0dd9eff52ee3220f5e6c7569b1fde2aa985008
Ngetich-86 <gideonkngetich86@gmail.com>
2024-12-17 00:29:53 +0300

    code refactor on tickets controllers

--- Backend/src/Ticketing/Ticketing.controller.ts
+++ Backend/src/Ticketing/Ticketing.controller.ts
@@ -6,7 +6,7 @@ import {
   updateTicket as updateTicketService,
   deleteTicket as deleteTicketService,
 } from './Ticketing.Service';
-
+//✅
 export const getAllTickets = async (c: Context) => {
   try {
     const tickets = await getAllTicketsService();
@@ -28,34 +28,45 @@ export const getTicketById = async (c: Context) => {
     return c.json({ message: 'Error fetching ticket', error }, 500);
   }
 };
-
+//create ticket✅
 export const createTicket = async (c: Context) => {
-  try {
-    const newTicket = await createTicketService(await c.req.json());
-    return c.json(newTicket, 201);
-  } catch (error) {
-    return c.json({ message: 'Error creating ticket', error }, 500);
-  }
-};
+  try{
+    const ticket = await c.req.json();
+    const createdTicket = await createTicketService(ticket);
+    if (createdTicket === undefined) return c.json({msg:"Ticket not created 😒 "}, 400);
+    return c.json(createdTicket, 201);        
+} catch (error: any) {
+    return c.text(error?.message, 400);
+}
+}
 
+//update ticket by ticket_id
 export const updateTicket = async (c: Context) => {
-  try {
-    const updatedTicket = await updateTicketService(Number(c.req.param('user_id')), await c.req.json());
-    if (updatedTicket) {
-      return c.json(updatedTicket, 200);
-    } else {
-      return c.json({ message: 'Ticket not found' }, 404);
-    }
-  } catch (error) {
-    return c.json({ message: 'Error updating ticket', error }, 500);
+  const ticketId = Number(c.req.param('id'));
+  try{
+    if(!ticketId) return c.text("Invalid ticket id😒", 400);
+    const ticket = await getTicketByIdService(ticketId);
+    if(ticket===undefined) return c.json({message: "No ticket found for this ticket id😒"},404);
+    const updatedTicket = await updateTicketService(ticketId, await c.req.json());
+    if (updatedTicket === undefined) return c.json({msg:"Ticket not updated 😒 "}, 400);
+    return c.json(updatedTicket, 200);
+  } catch(error){
+    return c.json({msg:"Error while updating ticket😒"})
   }
-};
+}
 
+
+//delete/cancel ticket by ticket_id
 export const deleteTicket = async (c: Context) => {
-  try {
-    await deleteTicketService(Number(c.req.param('id')));
-    return c.text('Ticket deleted', 204);
-  } catch (error) {
-    return c.json({ message: 'Error deleting ticket', error }, 500);
+  const ticketId = Number(c.req.param('id'));
+  try{
+    if(!ticketId) return c.text("Invalid ticket id", 400);
+    const ticket = await getTicketByIdService(ticketId);
+    if(ticket===undefined) return c.json({message: "No ticket found for this ticket id😒"},404);
+    const deletedTicket = await deleteTicketService(ticketId);
+    if (deletedTicket === undefined) return c.json({msg:"Ticket not deleted 😒 "}, 400);
+    return c.json({msg:deletedTicket}, 200);
+  } catch(error){
+    return c.json({msg:"Error while deleting ticket😒"})
   }
-};
+}
